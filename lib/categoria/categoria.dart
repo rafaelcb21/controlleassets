de07eaef93f17f8d717203aef462b0f9c549d797
rafaelcb21 <rafaelcb21@gmail.com>
2017-11-01 20:22:57 -0200

    version 0.0.5

--- lib/categoria/categoria.dart
+++ lib/categoria/categoria.dart
@@ -10,7 +10,7 @@ class CategoriaPage extends StatelessWidget {
         backgroundColor: azulAppbar,
         actions: <Widget>[
           new IconButton(
-            icon: const Icon(Icons.add_circle),
+            icon: const Icon(Icons.add),
             color: new Color(0xFFFFFFFF),
             onPressed: () {
               Navigator.of(context).push(new PageRouteBuilder(
@@ -55,6 +55,22 @@ class NovaCategoriaPage extends StatefulWidget {
 class NovaCategoriaPageState extends State<NovaCategoriaPage>{
   Color azulAppbar = new Color(0xFF26C6DA);
   String value = "Categoria principal";
+  Color colorEscolhida = new Color(0xFF000000);
+
+  void showCorDialog<T>({ BuildContext context, Widget child }) {
+    showDialog<T>(
+      context: context,
+      child: child,
+    )
+    .then<Null>((T value) { // The value passed to Navigator.pop() or null.
+      if (value != null) {
+        setState(() {
+          //_valueText = value.toString();
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold( 
@@ -82,43 +98,75 @@ class NovaCategoriaPageState extends State<NovaCategoriaPage>{
                 const Text("Subcategoria"),
               ],
             ),
-            new Row(
-              children: <Widget>[
-                new Icon(
-                  Icons.description,
-                  size: 24.0
-                ),
+            //new Row(
+            //  children: <Widget>[
                 new TextField(
                   maxLines: 1,
                   decoration: const InputDecoration(
+                    icon: const Icon(Icons.description),
                     labelText: "Nome",
-                    isDense: true,
                   ),
                   style: Theme.of(context).textTheme.title,
-                )
-              ],
-            ),
-            this.value == "Categoria principal" ?
+                ),
+            //  ],
+            //),
+            
             new Row(
               children: <Widget>[
                 new Icon(
                   Icons.palette,
-                  size: 24.0
+                  size: 24.0,
+                  color: Colors.black45,
                 ),
-                new TextField(
-                  maxLines: 1,
-                  decoration: const InputDecoration(
-                    labelText: "Cor",
-                    isDense: true,
-                  ),
-                  style: Theme.of(context).textTheme.title,
-                ),
-
+                new Text('Cor', style: new TextStyle(color: Colors.black38, fontSize: 20.0)),
+                new IconButton(
+                  icon: const Icon(Icons.lens),
+                  onPressed: () {
+                    showCorDialog<String>(
+                      context: context,
+                      child: new SimpleDialog(
+                        title: const Text('Selecione uma cor'),
+                        children: <Widget>[
+                          new Container(
+                            //margin: new EdgeInsets.only(left: 16.0, right: 16.0),
+                            child: new Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                new Container(
+                                  color: new Color(0xFFd10841),
+                                  height: 56.0,
+                                  width: 56.0,
+                                ),
+                                new Container(
+                                  color: new Color(0xFFcdd399),
+                                  height: 56.0,
+                                  width: 56.0,
+                                ),
+                                new Container(
+                                  color: new Color(0xFF87c0ec),
+                                  height: 56.0,
+                                  width: 56.0,
+                                ),
+                                new Container(
+                                  color: new Color(0xFF5aaeae),
+                                  height: 56.0,
+                                  width: 56.0,
+                                )                                
+                              ],
+                            ),    
+                          )
+                        ]
+                      )
+                    );
+                  },
+                )
+                //new Icon(
+                //  Icons.lens,
+                //  size: 24.0,
+                //  color: this.colorEscolhida,
+                //),
               ],
-            ):
-            
-              
-
+            )
           ],
         ),
       )
