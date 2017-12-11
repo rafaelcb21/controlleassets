2f4e8fd030e82d7466bc01b8f17649becffca3ae
rafaelcb21 <rafaelcb21@gmail.com>
2017-12-11 18:22:56 -0200

    continua layout lancamento page

--- lib/lancamento/consultalancamento.dart
+++ lib/lancamento/consultalancamento.dart
@@ -35,8 +35,18 @@ class ConsultaLancamentoPageState extends State<ConsultaLancamentoPage>{
       body: new ListView(
         padding: new EdgeInsets.only(top: 16.0),
         children: <Widget>[
+
+          ///Filtro
           new Container(
-            margin: new EdgeInsets.only(left: 16.0, right: 16.0, bottom: 12.0),
+            padding: new EdgeInsets.only(left: 16.0, right: 16.0, bottom: 12.0),
+            decoration: new BoxDecoration(
+              border: new Border(
+                bottom: new BorderSide(
+                  style: BorderStyle.solid,
+                  color: Colors.black12,
+                ),
+              )
+            ),
             child: new Row(
               children: <Widget>[
                 new Text(
@@ -50,59 +60,93 @@ class ConsultaLancamentoPageState extends State<ConsultaLancamentoPage>{
               ],
             ),
           ),
-          new Row(
-            children: <Widget>[
-              new Container(
-                margin: new EdgeInsets.only(left: 8.0),
-                child: new InkWell(
-                  onTap: (){},
-                  child: new Icon(
-                    Icons.keyboard_arrow_left,
-                    color: new Color(0xFF9E9E9E),
-                    size: 28.0,
-                  ),
-                ),
-              ),
-              new Expanded(
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new InkWell(
-                      onTap: (){},
-                      child: new Text(
-                        "Dezembro de 2017",
-                        style: new TextStyle(
-                          fontSize: 16.0,
-                        )
-                      ),
+
+          ///Mes
+          new Container(
+            padding: new EdgeInsets.only(bottom: 9.0, top: 9.0),
+            child: new Row(
+              children: <Widget>[
+                new Container(
+                  margin: new EdgeInsets.only(left: 8.0),
+                  child: new InkWell(
+                    onTap: (){},
+                    child: new Icon(
+                      Icons.keyboard_arrow_left,
+                      color: new Color(0xFF9E9E9E),
+                      size: 28.0,
                     ),
-                  ],
-                ),
-              ),
-              new Container(
-                margin: new EdgeInsets.only(right: 8.0),
-                child: new InkWell(
-                  onTap: (){},
-                  child: new Icon(
-                    Icons.keyboard_arrow_right,
-                    color: new Color(0xFF9E9E9E),
-                    size: 28.0,
                   ),
                 ),
-              ),
-            ],
+                new Expanded(
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new InkWell(
+                        onTap: (){},
+                        child: new Text(
+                          "Dezembro de 2017",
+                          style: new TextStyle(
+                            fontSize: 16.0,
+                          )
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(right: 8.0),
+                  child: new InkWell(
+                    onTap: (){},
+                    child: new Icon(
+                      Icons.keyboard_arrow_right,
+                      color: new Color(0xFF9E9E9E),
+                      size: 28.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
+
+          ///Dia
+          new Container(
+            padding: new EdgeInsets.only(left: 8.0, right: 8.0, bottom: 5.0, top: 5.0),
+            decoration: new BoxDecoration(
+              border: new Border(
+                bottom: new BorderSide(
+                  style: BorderStyle.solid,
+                  color: Colors.black12,
+                ),
+                top: new BorderSide(
+                  style: BorderStyle.solid,
+                  color: Colors.black12,
+                ),
+              )
+            ),
+            child: new Row(
+              children: <Widget>[
+                new Text(
+                  '1 de dezembro',
+                  style: new TextStyle(
+                    fontSize: 12.0,
+                    fontFamily: 'Roboto',
+                    color: new Color(0xFF9E9E9E)
+                  ),
+                ),
+              ],
+            ),
           ),
 
+          ///Lancamento
+          new ItemLancamento(),
           
         ]
       )
     );
   }
 }
-
-
-
 
 class ItemLancamento extends StatefulWidget {
    
@@ -199,11 +243,14 @@ class ItemLancamentoState extends State<ItemLancamento> with TickerProviderState
                   decoration: new BoxDecoration(
                     color: new Color(0xFFE57373),
                   ),
-                  child: new IconButton(
-                    icon: new Icon(Icons.delete),
-                    color: new Color(0xFFFFFFFF),
-                    onPressed: widget.onPressed3
-                  )
+                  padding: new EdgeInsets.all(12.0),
+                  child: new GestureDetector(
+                    onTap: (){},
+                    child: new Icon(
+                      Icons.delete,
+                      color: new Color(0xFFFFFFFF),
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -214,53 +261,92 @@ class ItemLancamentoState extends State<ItemLancamento> with TickerProviderState
               end: new Offset(this.abertura, 0.0),
             ).animate(_animation),
             child: new Container(
+              padding: new EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0, top: 8.0),
               decoration: new BoxDecoration(
+                color: new Color(0xFFFAFAFA),
                 border: new Border(
-                  top: new BorderSide(style: BorderStyle.solid, color: Colors.black26),
-                ),
-                color: new Color(0xFFFFFFFF),
+                  bottom: new BorderSide(
+                    style: BorderStyle.solid,
+                    color: Colors.black12,
+                  ),
+                )
               ),
-              margin: new EdgeInsets.only(top: 0.0, bottom: 0.0),
               child: new Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[          
+                children: <Widget>[
+                  new Container(
+                    padding: new EdgeInsets.only(right: 8.0),
+                    child: new Icon(
+                      Icons.brightness_1,
+                      color: new Color(0xFFE57373),
+                      size: 10.0,
+                    ),
+                  ),
                   new Expanded(
-                    child: new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: new InkWell(
+                      onTap: (){},
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Text(
+                            'Telefone fixo',
+                            overflow: TextOverflow.ellipsis,
+                            style: new TextStyle(
+                              fontSize: 14.0,
+                              fontFamily: 'Roboto',
+                              color: new Color(0xFF212121),
+                            ),
+                          ),
+                          new Text(
+                            'Telefonia',
+                            style: new TextStyle(
+                              fontSize: 12.0,
+                              fontFamily: 'Roboto',
+                              color: new Color(0xFF9E9E9E)
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  new Container(
+                    padding: new EdgeInsets.only(left: 8.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        new InkWell(
-                          onTap: widget.onPressed,
-                          child: 
-                            new Container(
-                              margin: new EdgeInsets.only(left: 16.0),
-                              padding: new EdgeInsets.only(right: 40.0, top: 4.5, bottom: 4.5),
-                              child: new Row(
-                                children: <Widget>[
-                                  new Container(
-                                    margin: new EdgeInsets.only(right: 16.0),
-                                    child: new Icon(
-                                      Icons.brightness_1,
-                                      color: widget.cor,
-                                      size: 35.0,
-                                    ),  
-                                  ),
-                                  new Text(
-                                    widget.tag,
-                                    style: new TextStyle(
-                                      color: Colors.black87,
-                                      fontSize: 14.0,
-                                      fontFamily: "Roboto",
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                              ],
-                            )
-                          )
+                        new Text(
+                          '-137,00',
+                          overflow: TextOverflow.ellipsis,
+                          style: new TextStyle(
+                            fontSize: 14.0,
+                            fontFamily: 'Roboto',
+                            color: new Color(0xFFE57373),
+                          ),
+                        ),
+                        new Text(
+                          'Pago',
+                          style: new TextStyle(
+                            fontSize: 12.0,
+                            fontFamily: 'Roboto',
+                            color: new Color(0xFF9E9E9E)
+                          ),
                         ),
                       ],
                     ),
-                  )
+                  ),
+                      
+                  new InkWell(
+                    onTap: (){},
+                    child: new Container(
+                      padding: new EdgeInsets.only(left: 10.0),
+                      child: new Icon(
+                        Icons.thumb_up,
+                        color: new Color(0xFF00BFA5),
+                        size: 24.0,
+                      ),
+                    ),
+                  )                
                 ],
               ),
             )
